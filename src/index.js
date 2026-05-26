51d0481a080ebcaf8b2493005176809906360404
Niranjan <niranjanvikasmulik2gmail.com>
2026-05-26 10:03:28 +0530

    Version 1

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log('index.js');
+console.log('version 1');
+//ls = list files and folders in the current folder
+//cd = change directory
+//git init = initialize git
+//git status = check version history of files
+//git add = add files for keeping version history
+//git add . = add all files in current directory
+//git commit -m "" = To know what we changed in this version
+//git log = to see the version history
